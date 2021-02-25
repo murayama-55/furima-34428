796fae2c3b3987e923e34a7d6d0cab16280d9556
murayama-55 <xx88o0o0@yahoo.co.jp>
2021-02-25 19:29:09 +0900

    Userモデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
   validates :birthday, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
-  
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
 end
