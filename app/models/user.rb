0e55a6039a412f2809b36a35d6007d167ea179be
murayama-55 <xx88o0o0@yahoo.co.jp>
2021-02-25 17:08:14 +0900

    passwordへのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :birthday, presence: true
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  
 end
