57d8ec49e6dae0f739e96af293b2a55b6799fdb1
murayama-55 <xx88o0o0@yahoo.co.jp>
2021-02-26 17:04:18 +0900

    itemsコントローラー（index,new,create）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,29 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new,:create]
 
   def index
+    @items = Item.all
   end
 
-  # private
+  def new
+    @item = Item.new
+  end
 
-  # def message_params
-    # params.require(:item).permit(:image)
-  # end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to :index
+    else
+      render new_item_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :item_name, :info, :price, :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :schedule_id, :image).merge(
+        user_id: current_user.id)
+  end
   
 end
