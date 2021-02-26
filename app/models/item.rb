304af87cf84ca85f969061c890c998e3e37c322e
murayama-55 <xx88o0o0@yahoo.co.jp>
2021-02-26 18:00:44 +0900

    active hash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :schedule
 
   with_options presence: true do
     validates :item_name
     validates :info
     validates :price
-    validates :category_id
-    validates :sales_status_id
-    validates :shipping_fee_id
-    validates :prefecture_id
-    validates :schedule_id
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :sales_status_id, numericality: { other_than: 1 } 
+    validates :shipping_fee_id, numericality: { other_than: 1 } 
+    validates :prefecture_id, numericality: { other_than: 1 } 
+    validates :schedule_id, numericality: { other_than: 1 } 
   end
 
   belongs_to :user
