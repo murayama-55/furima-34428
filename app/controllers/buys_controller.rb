ddb2c6eda66cf7a3a76507722eb4ab712ed2ad78
murayama-55 <xx88o0o0@yahoo.co.jp>
2021-03-06 21:00:41 +0900

    クレジットカード決済機能

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -7,19 +7,27 @@ class BuysController < ApplicationController
   end
 
   def create
-    #binding.pry
+    @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new(buy_params)
+    # binding.pry
     if @buy_address.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price,  # 商品の値段
+        card: buy_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
       @buy_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
+
   end
 
   private
   def buy_params
-    params.require(:buy_address).permit(:hoge, :post_code, :prefecture_id, :city, :address, :building, :phone).merge(item_id: params[:item_id])
+    params.require(:buy_address).permit(:post_code, :prefecture_id, :city, :address, :building, :phone).merge(item_id: params[:item_id], token: params[:token], user_id: current_user.id)
   end
 
 end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,8 +1,9 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :post_code, :prefecture, :city, :address, :building, :phone
+  attr_accessor :token, :item_id, :user_id, :post_code, :prefecture_id, :city, :address, :building, :phone
 
   with_options presence: true do
+    validates :token
     validates :item_id
     validates :user_id
     validates :post_code
@@ -18,7 +19,7 @@ class BuyAddress
   def save
     @buy = Buy.create(item_id: item_id, user_id: user_id)
     Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city,
-       address: address, building: building, phone: phone, buy_id: buy_id)
+       address: address, building: building, phone: phone)
   end
 
 end
